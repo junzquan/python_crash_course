20df49560f0ed44a35551c9902ca7d9541281d49
Cheng Chun Kuen <18507315+qmartinred@users.noreply.github.com>
2020-10-27 08:55:47 +0800

    Add some solutions to Chapter5

---
+++ Chapter5/5_1_conditional_tests.py
@@ -0,0 +1,30 @@
+car = 'bmw'
+print("Is car == 'bmw'? I predict True.")
+print(car == 'bmw')
+
+print("Is car == 'audi'? I predice False.")
+print(car == 'audi')
+
+# True Tests
+car = 'benz'
+print(car == 'benz')
+
+car = 'audi'
+print(car == 'audi')
+
+car = 'bmw'
+print(car == 'bmw')
+
+car = 'ford'
+print(car == 'ford')
+
+car = 'toyota'
+print(car == 'toyota')
+
+# False Tests
+car = 'bmw'
+print(car == 'benz')
+print(car == 'audi')
+print(car == 'toyota')
+print(car == 'subaru')
+print(car == 'ford')
---
+++ Chapter5/5_2_more_conditional_tests.py
@@ -0,0 +1,18 @@
+cars = ['Benz', 'BMW', 'Audi', 'Toyota', 'Ford']
+
+print(cars[0] == 'Benz')
+print(cars[0] == 'BMW')
+
+print(cars[1].lower() == 'bmw')
+
+print(3 > 2)
+print(1 < 2)
+print(3 >= 3)
+print(5 <= 5)
+
+print(3 > 2 and 1 < 3)
+print(3 > 2 or 2 > 3)
+
+print('BMW' in cars)
+
+print('Subaru' not in cars)
